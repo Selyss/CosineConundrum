02b0dfa84706a8d9cb2ffce8aa7ef5e3f3e4bdc7
Selyss <99344963+Selyss@users.noreply.github.com>
2024-01-17 21:46:29 -0500

    something

--- main.py
+++ main.py
@@ -2,14 +2,22 @@ import click
 
 @click.command()
 @click.argument('input_file', type=click.Path(exists=True, readable=True), required=True)
-def generate(input_file):
+def main(input_file):
     with open(input_file, 'r') as file:
         lines = file.readlines()
     
     if len(lines) != 0:
         click.echo("Error: File should be blank.")
         return
+    
+    click.echo("Success: Valid file.")
+    with open(input_file, 'w') as file:
+        file.write("\\[{\\sin\\theta}\\]") 
+
+    click.echo("Success: File modified.")   
+
+
 
 if __name__ == '__main__':
-    generate()
+    main()
 
