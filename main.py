d477ada4b6f2bb39a4f0d52968ec2fdf89b294e1
Selyss <99344963+Selyss@users.noreply.github.com>
2024-01-17 23:01:38 -0500

    more update

--- main.py
+++ main.py
@@ -1,23 +1,72 @@
 import click
+import json
+import random
+
+# LaTeX matching for primary stuff
+sine = "\\sin\\theta"
+sinesqrd = "\\sin^2\\theta"
+consine = "\\cos\\theta"
+consinesqrd = "\\cos^2\\theta"
+tangent = "\\tangent\\theta"
+tangentsqrd = "\\tangent^2\\theta"
+
+
+def random_sine():
+    # open config.json
+    with open("config.json", "r") as file:
+        data = json.load(file)
+
+    rand = random.choice(data["sine"])
+    return rand
+
+
+def random_sinesqrd():
+    # open config.json
+    with open("config.json", "r") as file:
+        data = json.load(file)
+
+    rand = random.choice(data["sinesquared"])
+    return rand
+
+
+def expand(input_file):
+    with open(input_file, "r") as file:
+        lines = file.readlines()
+
+    for line in lines:
+        if sine in line:
+            val = random_sine()
+            line = line.replace(sine, val)
+
+        if sinesqrd in line:
+            val = random_sinesqrd()
+            line = line.replace(sinesqrd, val)
+
+    with open(input_file, "w") as file:
+        file.writelines(lines)
+
+    click.echo("Success: Equations expanded.")
+
 
 @click.command()
-@click.argument('input_file', type=click.Path(exists=True, readable=True), required=True)
+@click.argument(
+    "input_file", type=click.Path(exists=True, readable=True), required=True
+)
 def main(input_file):
-    with open(input_file, 'r') as file:
+    with open(input_file, "r") as file:
         lines = file.readlines()
-    
+
     if len(lines) != 0:
         click.echo("Error: File should be blank.")
         return
-    
+
     click.echo("Success: Valid file.")
-    with open(input_file, 'w') as file:
-        file.write("\\[{\\sin^2\\theta}={\\sin^2\\theta}\\]") 
+    with open(input_file, "w") as file:
+        file.write("\\[{\\sin^2\\theta}={\\sin^2\\theta}\\]")
 
-    click.echo("Success: File modified.")   
+    click.echo("Success: File modified.")
+    expand(input_file)
 
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
-
